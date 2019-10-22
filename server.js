15cd8e03407c1d0b41d4644c2c42e80b496cfcb7
mblavatsky-at-222784684405 <mblavatsky@guesswho.com>
2019-10-22 20:46:55 +0400

    server js updated

--- server.js
+++ server.js
@@ -4,32 +4,36 @@ const env = require('get-env')();
 const brouter = require('./blogs');
 const mongoose = require('mongoose');
 const blogSchema = require('./blogSchema');
-const Blog = mongoose.model('blog', blogSchema);
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 let myBlog = new Blog(
   { title: String, author: String, body: String },
-  { collection: 'blog' }
+  { collection: 'Blog' }
 );
 
 app.use(brouter);
 const port = process.env.PORT || 3000;
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log(`Example app listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`listening on port ${port}!`));
 
-mongoose.connect(
-  'mongodb+srv://blava123:blava123@blavasclusta-po9fd.mongodb.net/test?retryWrites=true&w=majority',
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose
+  .connect('mongodb+srv://blava123:blava123@cluster0-po9fd.mongodb.net/Blog', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .catch(error => {
+    console.log(error);
+    handleError(error);
+  });
 
-const connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'connection error:'));
-connection.once('open', function() {
-  console.log('Connected!');
-  const query = new mongoose.Query();
-  query.collection = Blog.collection;
-  query.where('author', 'Mikheil Blavatsky').exec(elem => {
-    console.log(elem);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function() {
+  console.log('Connected....');
+
+  Blog.find({ author: 'blava' }, (err, blo) => {
+    console.log(blo);
   });
 });
